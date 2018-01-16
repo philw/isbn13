d4905bde64024d9ebe9e3eb632790d1286080d12
Phil Wood <phil@pwood.net>
2018-01-16 01:04:38 +0000

    first version - check isbn is correct

--- isbn13/Module1.vb
+++ isbn13/Module1.vb
@@ -2,6 +2,27 @@
 
     Sub Main()
 
+        Dim ISBN As String
+        Dim Digit As Integer
+        Dim Total As Integer
+
+        Console.Write("Enter isbn: ")
+        ISBN = Console.ReadLine
+
+        Total = 0
+        For i = 0 To ISBN.Length - 1
+            Digit = Asc(ISBN(i)) - Asc("0")
+            If i Mod 2 = 0 Then
+                Total += Digit
+            Else
+                Total += Digit * 3
+            End If
+        Next
+
+        Console.WriteLine(Total Mod 10)
+
+        Console.ReadLine()
+
     End Sub
 
 End Module
